70e03e6de67a5a062a8717f87bdeb497118e3aa8
刘关发 <triste_liu@163.com>
2023-07-04 10:05:32 +0800

    caller

--- log/utils/utils.go
+++ log/utils/utils.go
@@ -3,26 +3,12 @@ package utils
 import (
 	"fmt"
 	"github.com/sirupsen/logrus"
-	"os"
-	"path/filepath"
 	"strings"
 )
 
 func EntryCallerHandler(entry *logrus.Entry) (caller string) {
-	wd, err := os.Getwd()
-	if err != nil {
-		return ""
-	}
-	goPath := os.Getenv("GOPATH")
-	goPath = strings.Replace(goPath, "\\", "/", -1)
-	goPath = goPath + "/pkg/mod/"
-
-	parentDir := filepath.Dir(filepath.Clean(wd))
-	parentDir = strings.Replace(parentDir, "\\", "/", -1)
-	file := strings.TrimPrefix(entry.Caller.File, parentDir+"/")
-	file = strings.TrimPrefix(file, goPath)
 	funcSplit := strings.Split(entry.Caller.Function, "/")
 	function := funcSplit[len(funcSplit)-1]
-	caller = fmt.Sprintf("%s:%s:%d", file, function, entry.Caller.Line)
+	caller = fmt.Sprintf("%s(%s:%d)", function, entry.Caller.File, entry.Caller.Line)
 	return
 }
