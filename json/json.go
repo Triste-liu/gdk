a6d229c691bc5a227c6af30f6800ffe4d581140c
刘关发 <triste_liu@163.com>
2023-11-28 10:58:15 +0800

    json

---
+++ json/json.go
@@ -0,0 +1,15 @@
+package json
+
+import (
+	"encoding/json"
+	"github.com/triste-liu/gdk/log"
+)
+
+func ToString(v any) string {
+	marshal, err := json.Marshal(v)
+	if err != nil {
+		log.Warning(err)
+		return ""
+	}
+	return string(marshal)
+}
